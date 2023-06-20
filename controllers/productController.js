dc65160bfb2bfc96d8491bbdbc669db667509130
treeoon <wisnuyeahfaizal@gmail.com>
2023-06-20 18:58:26 +0700

    feat: all controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,78 @@
-const Product = require('../models/Product');
-
+const Product = require("../models/Product");
+const productId = req.params.id;
 
 // Get all products
-const getProducts = async (req, res) => {};
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({
+      message: "get products success",
+      data: products
+    })
+  } catch (err) {
+    res.status(500).json({
+      message: "get products success",
+      data: err
+    })
+  }
+};
 
 // Save a product
-const saveProduct = async (req, res) => {};
+const saveProduct = async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    const savedProduct = await product.save();
+    res.status(200).json({
+      message: "post product success",
+      data: savedProduct
+    })
+  } catch (err) {
+    res.status(200).json({
+      message: "post product success",
+      data: err
+    })
+  }
+};
 
 // Update a product
-const updateProduct = async (req, res) => {};
+const updateProduct = async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json({
+      message: "update product success",
+      data: updatedProduct,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "update product success",
+      data: err,
+    });
+  }
+};
 
 // Delete a product
-const deleteProduct = async (req, res) => {};
+const deleteProduct = async (req, res) => {
+  try {
+    const deleteProduct = await Product.findByIdAndDelete(productId, req.body);
+    res.status(200).json({
+      message: "delete product success",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "delete product failed",
+    });
+  }
+};
 
 module.exports = {
   getProducts,
   saveProduct,
   updateProduct,
-  deleteProduct
-}
+  deleteProduct,
+};
